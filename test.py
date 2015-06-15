653fd14a11b9680d6dae2f6cfd88756dea19f380
César Lara <laralacesar@gmail.com>
2015-06-15 18:56:57 -0500

    All python code linted according to pep8

--- test.py
+++ test.py
@@ -1,29 +1,28 @@
 from MailjetApi import MailjetApi
 
-obj = MailjetApi(user = '', password = '')
+obj = MailjetApi(user='', password='')
 
 # ListID = 19
 
 # Create a new contact
-#print obj.create_contact(email='dssgfdoer@yahoo.com.mx',name='ggmfdi')
+# print obj.create_contact(email='dssgfdoer@yahoo.com.mx',name='ggmfdi')
 
 # Create a new list contacts
-#print obj.create_contactlist(address='yo@dharwinperez.com',name='Sparky-Children')
+# print obj.create_contactlist(
+# address='yo@dharwinperez.com',name='Sparky-Children')
 # Add contact to list
-#print obj.add_contact_to_list( contact_id = obj.currentUser, list_id = obj.currentList )
+# print obj.add_contact_to_list(
+# contact_id = obj.currentUser, list_id = obj.currentList )
 
-# View a contactlist 
+# View a contactlist
 
 # print obj.view_contactlist( contactslist_id = 3)
 
 print obj.send_email(
-			fromm 	= 	'yo@dharwinperez.com',
-			to 		=	'dhararon@hotmail.com',
-			subject =	'Mailjet Python SDK',
-			message = 	'''This is the link to github. 
-
-			https://github.com/sparkyguardian/mailjet-python-sdk
-							
-			Send with mailjet python sdk :D
-						'''		
-		)
+	fromm='yo@dharwinperez.com',
+	to='dhararon@hotmail.com',
+	subject='Mailjet Python SDK',
+	message='''This is the link to github.
+	https://github.com/sparkyguardian/mailjet-python-sdk
+	Send with mailjet python sdk :D
+	''')
